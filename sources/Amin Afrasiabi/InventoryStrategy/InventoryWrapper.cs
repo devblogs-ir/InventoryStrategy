bc2dc390f29c9f1e4ce906b428b95ec1f4e89bad
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-01-04 00:56:52 +0330

    Implement GetEnumerator Method

---
+++ sources/Amin Afrasiabi/InventoryStrategy/InventoryWrapper.cs
@@ -0,0 +1,36 @@
+﻿using InventoryStrategy.Enumerators;
+using InventoryStrategy.Models;
+using System.Collections;
+
+namespace InventoryStrategy;
+
+public class InventoryWrapper<T>(InventoryManagementApproach managementApproach) : IEnumerable<T>
+{
+    public InventoryManagementApproach _managementApproach = managementApproach;
+    public List<T> Products = [];
+
+    public void Add(T item)
+    {
+        Products.Add(item);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        IEnumerator<T> enumeratorStrategy = null!;
+        switch (_managementApproach)
+        {
+            case InventoryManagementApproach.LastInFirstOut:
+                enumeratorStrategy = new LIFOEnumerator<T>(Products);
+                break;
+            case InventoryManagementApproach.FirstInFirstOut:
+                enumeratorStrategy = new FIFOEnumerator<T>(Products);
+                break;
+        }
+        while (enumeratorStrategy.MoveNext())
+        {
+            yield return enumeratorStrategy.Current;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
